7994eff853066de79dd1b8de4c52d2902f939894
Alexander Legeyda <alex@legeyda.com>
2022-10-06 19:09:50 +0300

    refactor

--- saturn-monitoring.sh
+++ saturn-monitoring.sh
@@ -8,9 +8,75 @@ set -eu
 : "${TARGET_METHOD:=GET}"
 : "${TARGET_REQUIRE_2XX:=true}"
 : "${CHECK_INTERVAL:=60}"
-: "${CHECK_DAILY_PING:=true}"
+: "${RESEND_TIMEOUT:=$[24*60*60]}"
 
 
+
+function main() {
+	trap on-exit EXIT
+
+	local send_timestamp=0
+	local previous_status=undefined
+
+	send-message "👁 $TARGET_NAME monitoring goes up"
+	while true; do
+		local check_details
+		if check_details="$(check)"; then
+			if [[ up != "$previous_status" ]] || is-resend-timeout; then
+				send-message "🚀 $TARGET_NAME is available"
+				send_timestamp=$(timestamp)
+				previous_status=up
+			fi
+		else
+			if [[ down != "$previous_status" ]] || is-resend-timeout; then
+				send-message "💀 $TARGET_NAME unavailable! Request $TARGET_METHOD $TARGET_URL failed, details below.%0A%0A$check_details"
+				send_timestamp=$(timestamp)
+				previous_status=down
+			fi
+		fi
+
+		sleep "$CHECK_INTERVAL"
+	done
+}
+
+function on-exit() {
+	send-message "🙈 $TARGET_NAME monitoring goes down"
+}
+
+# env: TELEGRAM_API_KEY
+#      TELEGRAM_CHAT_ID
+#      send_timestamp
+function send-message() {
+	local text="$@"
+	curl -X POST "https://api.telegram.org/bot$TELEGRAM_API_KEY/sendMessage" --data "chat_id=$TELEGRAM_CHAT_ID&text=${text:0:4096}" || true
+}
+
+# env: RESEND_TIMEOUT
+#      send_timestamp
+function is-resend-timeout() {
+	[[ $RESEND_TIMEOUT -le 0 ]] && return 1
+	#[[ $(now) < $(today)_12 ]] && return 1
+	#[[ $(now) > $(today)_20 ]] && return 1
+	(( $(timestamp) <= $send_timestamp + $RESEND_TIMEOUT )) && return 1
+	return 0
+}
+
+# env: TARGET_METHOD
+#      TARGET_URL
+#      TARGET_REQUIRE_2XX
+function check() {
+	local curl_args=''
+	if [[ GET != "$TARGET_METHOD" ]]; then
+		curl_args="-X $TARGET_METHOD"
+	fi
+
+	curl --verbose $curl_args "$TARGET_URL" 2>&1 || return 1
+	if [[ "$TARGET_REQUIRE_2XX" != 'false' ]]; then
+		echo "$check_details" | grep -Eqx '< HTTP/[^ ]+ 2[0-9]+' || return 1
+	fi
+
+	return 0
+}
 
 function today() {
 	date +%Y-%m-%d
@@ -20,47 +86,29 @@ function now() {
 	date +%Y-%m-%d_%H-%M-%S
 }
 
-CHECK_DETAILS=undefined
-PREVIOUS_STATUS=undefined
-PREVIOUS_PING_DATE=$(today)
-
-
-function check() {
-	CHECK_DETAILS=$(curl --verbose -X "$TARGET_METHOD" "$TARGET_URL" 2>&1) || return 1
-	if [[ "$TARGET_REQUIRE_2XX" != 'false' ]]; then
-		echo "$CHECK_DETAILS" | grep -q '< HTTP/1.1 2' || return 1
-	fi
+function timestamp() {
+	date +%s
 }
 
-function send-message() {
-	local text="$@"
-	curl -X POST "https://api.telegram.org/bot$TELEGRAM_API_KEY/sendMessage" --data "chat_id=$TELEGRAM_CHAT_ID&text=${text:0:4096}" || true
+
+# ================================
+
+# env: DEBUG?
+#      BASH_SOURCE?
+entry-point() {
+	set -eu
+	if [[ true == "${DEBUG:-}" ]]; then
+		set -x
+	fi
+	local script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+	script_dir="$script_dir" main "$@"
 }
 
-function on-exit() {
-	send-message "🙈 $TARGET_NAME monitoring goes down"
+# env: BASH_SOURCE
+#      0
+function script-is-sourced() {
+	# todo https://unix.stackexchange.com/a/215279
+    test "${BASH_SOURCE[0]}" != "${0}" || return 1
 }
-trap on-exit EXIT
 
-send-message "👁 $TARGET_NAME monitoring goes up"
-while true; do
-	if check; then
-		if [[ up != "$PREVIOUS_STATUS" ]]; then
-			send-message "🚀 $TARGET_NAME is available"
-			PREVIOUS_STATUS=up
-		fi
-	else
-		if [[ down != "$PREVIOUS_STATUS" ]]; then
-			send-message "💀 $TARGET_NAME unavailable! Request $TARGET_METHOD $TARGET_URL failed, details below.%0A%0A$CHECK_DETAILS"
-			PREVIOUS_STATUS=down
-		fi
-	fi
-
-	if [[ "$CHECK_DAILY_PING" == 'true' ]] && [[ $PREVIOUS_PING_DATE != $(today) ]] && [[ $(now) > $(today)_12 ]]; then
-		send-message "👌 $TARGET_NAME monitoring goes on"
-		PREVIOUS_PING_DATE=$(today)
-	fi
-
-	CHECK_DETAILS=undefined
-	sleep "$CHECK_INTERVAL"
-done
+entry-point "$@"
